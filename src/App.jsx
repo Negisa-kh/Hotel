5eabfc8960b858d6078c1c462133e52a5050e2cf
Negisa-kh <139978798+Negisa-kh@users.noreply.github.com>
2025-02-06 12:03:04 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import Header from "./component/header/Header";
 import Boody from "./component/Boody/Boody"
 import Footer from "./component/Footer/Footer"
