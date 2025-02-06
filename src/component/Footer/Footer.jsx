82b2ee156ffcf111eef818ca868852cd96fc7db8
Negisa-kh <139978798+Negisa-kh@users.noreply.github.com>
2025-02-06 11:59:23 +0330

    Update Footer.jsx

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="mx-auto max-w-screen-xl">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com" className="flex items-center">
+              <a href="#" className="flex items-center">
                 <img
                   src={logo}
                   className="mr-3 h-8"
